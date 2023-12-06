ed6bb45444cc64972f6cb33e6e474be79cf93e92
RED-HOODIES <152661993+RED-HOODIES@users.noreply.github.com>
2023-12-06 02:53:30 -0500

    Add files via upload

---
+++ DISPLAY_7SEG/PIC18F4550_DISPLAY_7SEG.X/Setup.h
@@ -0,0 +1,30 @@
+
+#include <xc.h>
+
+#define F_CPU 1000000UL //Para usar el delay
+
+void SetupOscilador(void);
+void Setupports(void);
+void Setupint(void);
+void SetupADC(void);
+void Setup(void);
+
+
+#ifndef SETUP_H
+#define	SETUP_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SETUP_H */
+
+
+
